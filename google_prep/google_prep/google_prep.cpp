ec1592418bbeaf7da236b29562ae246381a32cf5
Joseph <jkb243@cornell.edu>
2015-04-21 23:17:26 -0600

    google_prep

---
+++ google_prep/google_prep/google_prep.cpp
@@ -0,0 +1,13 @@
+// google_prep.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("This is the template for the google prep code framework");
+	getchar();
+	return 0;
+}
+
