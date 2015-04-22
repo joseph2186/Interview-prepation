9ef0c99756afe8beacb79c5daee77f0602d9c73c
Joseph <jkb243@cornell.edu>
2015-04-22 00:02:09 -0600

    merge sort

--- google_prep/google_prep/google_prep.cpp
+++ google_prep/google_prep/google_prep.cpp
@@ -2,12 +2,25 @@
 //
 
 #include "stdafx.h"
+#include "stdlib.h"
+#include "merge_sort.cpp"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf("This is the template for the google prep code framework");
+	int a[20], i, b[20];
+	cout<<"enter  the elements\n";
+	for (i = 0; i < 5; i++)
+	{
+        cin>>a[i];
+	}
+    mergesort(a, 0, 4);
+	cout<<"sorted array\n";
+    for (i = 0; i < 5; i++)
+	{
+        cout<<a[i];
+	}
+
 	getchar();
 	return 0;
 }
-
